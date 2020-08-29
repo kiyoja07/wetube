0ad108beda46cb6d9d0ed76bb0db2ce4d6205449
YongJaeKim <kiyoja07@gmail.com>
2020-08-29 17:14:53 +0900

    5-2.everythin else

--- middlewares.js
+++ middlewares.js
@@ -7,10 +7,10 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
-    id: 1
+    isAuthenticated: false,
+    id: 1,
   };
   next(); // next() 함수를 호출하면 앱 내의 그 다음 미들웨어 함수가 호출된다
 };
 
-export const uploadVideo = multerVideo.single("videoFile");  // single: 하나의 파일만 업로드, videoFile: upload.pug의 video input file name
+export const uploadVideo = multerVideo.single("videoFile"); // single: 하나의 파일만 업로드, videoFile: upload.pug의 video input file name
