04db7611b0d62063dbf6b3226059e301d22c46eb
YongJaeKim <kiyoja07@gmail.com>
2020-09-05 15:22:19 +0900

    6-5.MongoStore and Middlewares

--- middlewares.js
+++ middlewares.js
@@ -6,11 +6,24 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.user = {
-    isAuthenticated: false,
-    id: 1,
-  };
+  res.locals.user = req.user || null;
   next(); // next() 함수를 호출하면 앱 내의 그 다음 미들웨어 함수가 호출된다
+};
+
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
 };
 
 export const uploadVideo = multerVideo.single("videoFile"); // single: 하나의 파일만 업로드, videoFile: upload.pug의 video input file name
