25cf26d67b064dcad2fd25bc63f1130eda037777
YongJaeKim <kiyoja07@gmail.com>
2020-11-13 13:33:14 +0900

    11.7 Deploying to Heroku part Two

--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://quiet-brook-20620.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`,
       scope: "user:email",
     },
     githubLoginCallback
